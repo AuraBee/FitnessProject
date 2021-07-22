efa9297eb26b5395fd8c9475f87b7590f98c535a
Be Sky <skyaze@outlook.com>
2021-07-22 14:25:20 +0100

    refactored classes&styling

--- index.js
+++ index.js
@@ -6,10 +6,27 @@ let open = get("menu-btn");
 let nav = get("nav");
 let exit = get("exit-btn");
 
-open.addEventListener('click', (event) => {
+open.addEventListener('click', () => {
     nav.classList.add('open-nav');
 })
 
 exit.addEventListener('click', () => {
     nav.classList.remove('open-nav');
 })
+
+// /** 
+//   interactive buttons: Open Modal 
+//   **/
+
+// document.getElementById("open-modal").addEventListener("click", function() {
+//     document.getElementById("overlay").style.display = "block";
+// })
+
+
+// /** 
+//   Interactive buttons: Close Modal
+// **/
+
+// document.getElementById("close-modal").addEventListener("click", function() {
+//     document.getElementById("overlay").style.display = "none";
+// })
